50087ac433b911d66bfa6e430cbe53c445993afa
Ding <ding.chen@cloud-interactive.com>
2020-09-18 10:48:54 +0800

    Modify the index.d.ts file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,10 @@
-export type Result = {
-  item: string;
-  highlight: Boolean;
+declare type Result = {
+    item: string;
+    highlight: Boolean;
 };
-
-export type mainProps = (
-  text: string,
-  query: string,
-  options?: { delimiter?: string; caseSensitive?: boolean }
-) => Result[];
-
-export type parseProps = (
-  text: string,
-  queryList: string[],
-  options?: { delimiter?: string; caseSensitive?: boolean }
-) => Result[];
+declare type mainProps = (text: string, query: string, options?: {
+    delimiter?: string;
+    caseSensitive?: boolean;
+}) => Result[];
+declare const main: mainProps;
+export default main;
